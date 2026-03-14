9c4e48ca5631b21022df144a57489e789ecfe6a4
chaimete-debug <chaimete@gmail.com>
2026-03-14 17:57:21 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,11 @@
+self.addEventListener("install", () => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", () => {
+  // cache desactivada temporariamente
+});
